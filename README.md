82b9188aa2cbbe5b49a4947de7c27e6d453809a7
Marlene Moraes <marlenevmoraes@outlook.com>
2021-12-19 00:39:27 -0300

    update project

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# pessoas-de-tech
+# Pessoas de Tech
 Repositório destinado aos desafios do servidor Pessoas de Tech, para avaliação dos administradores seniores. Os desafios estão separados por pastas, para melhor organização dos projetos e do meu perfil. 
 
 Sendo assim, o repositório consta:
-[ ] Desafio 01 - site MeuFreela
+[ ]- Desafio 01 - site MeuFreela
 
 ## Tecnologias utilizadas
 Angular em aplicações front-end e Java em aplicações back-end.
--- desafio01/src/app/app.module.ts
+++ desafio01/src/app/app.module.ts
@@ -4,18 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './component/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MainComponent } from './component/main/main.component';
+import { HeaderComponent } from './component/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    MainComponent
+    HeaderComponent
   ],
   
   imports: [
